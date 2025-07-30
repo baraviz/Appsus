66c91cde39d49bedef52479e8d3887d9ebfb7c64
Itaish-Bzu <Itaish94@gmail.com>
2025-07-30 11:28:49 +0300

    home

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -2,7 +2,7 @@ import { showSuccessMsg } from '../services/event-bus.service.js'
 
 export function Home() {
     return <section className="container home">
-        <h1>Welcome Appsus</h1>
+        <h1>Welcome Home</h1>
         <button onClick={() => showSuccessMsg('Yep, that works')}>Show Msg</button>
         <div className="box-container">
             <div className="box1"></div>
