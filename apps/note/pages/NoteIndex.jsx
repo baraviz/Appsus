ec0d40493d547205e3dc62d088fd66e7f36bfee2
Itaish-Bzu <Itaish94@gmail.com>
2025-07-30 12:12:50 +0300

    Create basic note service

--- apps/note/pages/NoteIndex.jsx
+++ apps/note/pages/NoteIndex.jsx
@@ -1,4 +1,12 @@
+import { noteService } from "../../../services/note.service.js"
 
+noteService
 export function NoteIndex() {
+
+    noteService.query()
+    .then(notes=>console.log(notes))
+
+
+
     return <section className="container">Notes app</section>
 }
---
+++ services/note.service.js
@@ -0,0 +1,67 @@
+import { storageService } from "./async-storage.service.js"
+import { utilService } from "./util.service.js"
+
+
+const NOTE_KEY = 'noteDB'
+
+export const noteService = {
+    query,
+    get,
+    remove,
+    save,
+}
+
+
+function query() {
+    return storageService.query(NOTE_KEY)
+}
+
+function get(noteId) {
+    return storageService.get(NOTE_KEY, noteId).then(_setNextPrevNoteId)
+}
+
+function remove(noteId) {
+    return storageService.remove(NOTE_KEY, noteId)
+}
+
+function save(note) {
+    if (note.id) {
+        return storageService.put(NOTE_KEY, note)
+    } else {
+        return storageService.post(NOTE_KEY, note)
+    }
+}
+
+const notes = [
+    {
+        id: 'n101',
+        createdAt: 1112222,
+    type: 'NoteTxt',
+    isPinned: true,
+    style: { backgroundColor: '#00d' },
+    info: { txt: 'Fullstack Me Baby!' },
+  },
+  {
+      id: 'n102',
+      createdAt: 1112223,
+      type: 'NoteImg',
+      isPinned: false,
+      info: { url: 'http://some-img/me', title: 'Bobi and Me' },
+    style: { backgroundColor: '#00d' },
+},
+  {
+    id: 'n103',
+    createdAt: 1112224,
+    type: 'NoteTodos',
+    isPinned: false,
+    info: {
+        title: 'Get my stuff together',
+        todos: [
+            { txt: 'Driving license', doneAt: null },
+            { txt: 'Coding power', doneAt: 187111111 },
+      ],
+    },
+  },
+]
+
+    utilService.saveToStorage(NOTE_KEY,notes)
