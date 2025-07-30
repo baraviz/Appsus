6e1f91e4169aadce20f6baa7fb36f749325823ef
baraviz <bar.avizemer@gmail.com>
2025-07-30 19:57:29 +0300

    show read/unread emails

--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -2,10 +2,10 @@ import { MailPreview } from "./MailPreview.jsx"
 
 export function MailList({ mails = [] }) {
     return (
-        <div>
-            <ul>
+        <div className="mail-list-container">
+            <ul className="mail-list">
                 {mails.map(mail => (
-                    <li key={mail.id}>
+                    <li key={mail.id} className={mail.isRead ? 'is-read' : ''}>
                         <MailPreview mail={mail} />
                     </li>
                 ))}
